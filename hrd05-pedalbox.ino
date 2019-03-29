72122416a30743df28ee15431ff5da14cf723c0b
GeneralX-NL <48676847+GeneralX-NL@users.noreply.github.com>
2019-03-29 15:11:59 +0100

    Fixed DIV0 error

--- hrd05-pedalbox.ino
+++ hrd05-pedalbox.ino
@@ -19,7 +19,7 @@
 #define BPS1_IN 2
 #define BPS2_IN 3
 
-int MAX_DIFF_MS = 100;
+//int MAX_DIFF_MS = 100;  // This parameter not used anymore; we always kill the car when we detect errors
 float MAX_DIFF = 0.075;
 
 int THROTTLE = 0;
@@ -30,7 +30,7 @@ int BRAKE    = 0;
 int POT_RATIO = 0.74;
 
 unsigned long last = 0;     //millis when last loop was executed
-int DATA_RATE = 20;          // Send CAN frame every 20 ms (and read sensors continiously)
+int DATA_RATE = 20;          // Send CAN frame every 20 ms (and read sensors continuously)
 
 unsigned long firstAPSErrorTime = 0;      //millis when the values were different for the first time
 unsigned long firstBPSErrorTime = 0;
@@ -40,6 +40,7 @@ bool first_BPS_Error = false;
 
 bool APS_Error = true;          //do a reading first to clear the error.
 bool BPS_Error = true;
+bool ERROR = true;
 
 byte BUFFERSIZE = 50
 int LPF_APS1[BUFFERSIZE];           //moving average APS sensor 1
@@ -47,10 +48,10 @@ int LPF_APS2[BUFFERSIZE];
 int LPF_BPS1[BUFFERSIZE];
 int LPF_BPS2[BUFFERSIZE];
 
-unsigned long AV_APS1 = 0;          //average value after filter for APS 1
-unsigned long AV_APS2 = 0;
-unsigned long AV_BPS1 = 0;
-unsigned long AV_BPS2 = 0;
+long AV_APS1 = 0;          //average value after filter for APS 1
+long AV_APS2 = 0;
+long AV_BPS1 = 0;
+long AV_BPS2 = 0;
 
 void setup() {
   //Get initial values to fill arrays:
@@ -77,8 +78,9 @@ void setup() {
 }
 
 void loop() {
-// Update moving average for pedal sensors:
-// Average to 0:
+  
+  // Update moving average for pedal sensors:
+  // Average to 0:
   AV_APS1 = 0;                        
   AV_APS2 = 0;
   AV_BPS1 = 0;
@@ -111,14 +113,38 @@ void loop() {
   AV_APS2 /= BUFFERSIZE;
   AV_BPS1 /= BUFFERSIZE;
   AV_BPS2 /= BUFFERSIZE;
-  THROTTLE = plausibility_check(AV_APS1, AV_APS2);
-  BRAKE = plausibility_check(AV_BPS1, AV_BPS2);
 
-  THROTTLE = map(THROTTLE, 784, 349, 0, 255);
-  THROTTLE = constrain(THROTTLE, 0, 255);
+  AV_APS1 = map(AV_APS1, 784, 349, 1, 1024)
+  AV_APS2 = map(AV_APS2, 784, 349, 1024, 1)
+  AV_BPS1 = map(AV_BPS1, 784, 349, 1, 1024)
+  AV_BPS2 = map(AV_BPS2, 784, 349, 1024, 1)
+  // Check for signal shorts to GND or 5V
+  if( AV_APS1 < 1 || AV_APS2 < 1 || AV_APS1 > 1024 || AV_APS2 > 1024 
+    AV_BPS1 < 1 || AV_BPS2 < 1 || AV_BPS1 > 1024 || AV_BPS2 > 1024){
+    ERROR = true;
+  }
+  else{
+    ERROR = false;
+  }
+  
+  // Check for signal plausibility
+  if( plausibility_check(AV_APS1, AV_APS2) || plausibility_check(AV_BPS1, AV_BPS2)){
+    ERROR = true;
+  }
+  else{
+    ERROR = false;
+  }
 
-  BRAKE = map(BRAKE, 784, 349, 0, 255);
-  BRAKE = constrain(BRAKE, 0, 255);
+  if(ERROR){
+    THROTTLE = 0;
+    BRAKE = 0;
+  }
+  else{
+    THROTTLE = map(AV_APS1, 1, 1024, 0, 255)/map(AV_APS2, 1, 1024, 0, 255);
+    THROTTLE = constrain(THROTTLE, 0, 255);
+    BRAKE = map(AV_BPS1, 1, 1024, 0, 255)/map(AV_BPS2, 1, 1024, 0, 255);
+    BRAKE = constrain(BRAKE, 0, 255);
+  }
 
   // Prevent BSPD from triggering
   // - 5kW from 66kW is 7.5%, so 19 of 255.
@@ -184,21 +210,18 @@ void loop() {
 //   }
 // }
 
-int plausibility_check(unsigned long POT1, unsigned long POT2) {
+bool plausibility_check(long POT1, long POT2) {
 
-  int OUT = 0;
+  bool IMPLAUSIBLE = true;
 
   float RATIO = (float)POT1 / (float)POT2;
   float DIFF = abs(RATIO - POT_RATIO);
 
   if (DIFF > MAX_DIFF) {
-    Serial.println("IMPLAUSIBLE SIGNAL");
-    THROTTLE = 0;
-    BRAKE = 0;
+    IMPLAUSIBLE = true;
   } else {
-    OUT = (POT1 + POT2) / 2;
+    IMPLAUSIBLE = false;
   }
-
-  return OUT;
+  return IMPLAUSIBLE;
 }
 
